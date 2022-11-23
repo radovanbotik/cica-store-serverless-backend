690872e6e302acf0e1952c2a46163f005e5ac898
radovanbotik <radovanbotik@gmail.com>
2022-11-23 16:54:12 +0100

    ''

---
+++ assets/data.js
@@ -0,0 +1,58 @@
+const storeProducts = [
+  {
+    id: "1",
+    name: "tshirt",
+    price: 999,
+    image:
+      "https://pyxis.nymag.com/v1/imgs/9aa/b7f/0b2bcd87ecdc80545622a833684a643539-white-tshirt.2x.rsquare.w600.jpg",
+    colors: ["#ff0000", "#00ff00", "#0000ff"],
+    description: "tshirt with an awesome picture of me",
+    category: "apparel",
+    shipping: true,
+    featured: true,
+  },
+  {
+    id: "2",
+    name: "mug",
+    price: 499,
+    image:
+      "https://www.coffeeart.sk/sub/coffeeart.sk/shop/product/coffeeart-mug-white-025-l-1301.jpg",
+    colors: ["#000", "#ffb900"],
+    description: "mug with an awesome picture of me",
+    category: "accessories",
+    featured: true,
+  },
+  {
+    id: "3",
+    name: "sweatpants",
+    price: 1999,
+    image:
+      "https://img.hollisterco.com/is/image/anf/KIC_347-2683-0809-112_prod1?policy=product-medium",
+    colors: ["#000", "#ffb900"],
+    description: "sweatpants with an awesome picture of me",
+    category: "apparel",
+  },
+  {
+    id: "4",
+    name: "bandana",
+    price: 899,
+    image:
+      "https://www.gangstagroup.sk/sub/gangstagroup.com/shop/product/urban-classics-bandana-3-pack-black-87392.jpg",
+    colors: ["#000", "#ffb900"],
+    description: "bandana with an awesome picture of me",
+    category: "accessories",
+    featured: true,
+  },
+  {
+    id: "5",
+    name: "vinyl record",
+    price: 1999,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrIZls0VHBmOrtaeoakb51PzHQwiY553sduQU7h23hvS9sf-O7CX1KAoVkeDkUJ5a2hLY&usqp=CAU",
+    description: "vinyl record of me",
+    category: "entartaiment",
+    featured: true,
+  },
+];
+
+module.exports = storeProducts;
---
+++ index.js
@@ -0,0 +1,28 @@
+const productsContainer = document.querySelector(".products-container");
+
+const fetchData = async () => {
+  try {
+    // const resp = await fetch("/.netlify/functions/1-hello/");
+    const resp = await fetch("/api/cicaStore/");
+    const data = await resp.json();
+    const UI = data
+      .map(entry => {
+        return `
+        <article class='product'>
+        <h2>name:${entry.name}</h2>
+        <h3>id:${entry.id}</h3>
+        <h4>price:${entry.price}</h4>
+        <h4>category:${entry.category}</h4>
+        <h5>description:${entry.description}</h5>
+        <img src="${entry.image}"/>
+        </article>
+        `;
+      })
+      .join("");
+    productsContainer.innerHTML = UI;
+  } catch (error) {
+    productsContainer.innerHTML = `<h1>there was an error.</h1>`;
+  }
+};
+
+fetchData();
