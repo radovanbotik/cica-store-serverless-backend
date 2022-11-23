eb65660e303c177b9c57f3334d0844c31d298593
radovanbotik <radovanbotik@gmail.com>
2022-11-23 17:15:09 +0100

    ''

--- functions/cicaStore.js
+++ functions/cicaStore.js
@@ -3,6 +3,9 @@ const products = require("../assets/data.js");
 
 exports.handler = async (event, context) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(products),
   };
