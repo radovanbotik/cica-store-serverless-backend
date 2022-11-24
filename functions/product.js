7b7220c9c948b1b15b355d74c4d4a489cc5bd4d8
radovanbotik <radovanbotik@gmail.com>
2022-11-24 14:41:28 +0100

    ''

--- functions/product.js
+++ functions/product.js
@@ -19,6 +19,9 @@ exports.handler = async (event, context) => {
         };
       }
       return {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         statusCode: 200,
         body: JSON.stringify(product),
       };
